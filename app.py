80be195f67a2dc7c4a35b8f1e7a85c38f277743c
Atsushi <kakuta.atsushi@outward.jp>
2023-01-21 06:05:17 +0900

    tiny fix routing

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ from common.tweet import tweet_run
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/tweet', methods=['POST'])
-def test():
     if request.method == 'POST':
         tweet_run(request.form['tweet_body'])
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
