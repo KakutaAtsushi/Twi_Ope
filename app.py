f947c857352449fe50cfbf8113514655ef9bc660
Atsushi <kakuta.atsushi@outward.jp>
2023-01-21 06:12:11 +0900

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
 from common.tweet import tweet_run
 
 app = Flask(__name__)
@@ -10,6 +10,17 @@ def hello_world():
         tweet_run(request.form['tweet_body'])
         return redirect(url_for('index'))
     return render_template('index.html')
+
+
+@app.errorhandler(500)
+def not_found(error):
+    #エラーJSON作成
+    result = {
+        "error": error,
+        "result":False
+        }
+    #エラーJSONを出力
+    return make_response(jsonify(result), 500)
 
 
 if __name__ == '__main__':
