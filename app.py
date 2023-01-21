522f1ee1f628365dcc54044ca1e6c7e99394b08b
Atsushi <kakuta.atsushi@outward.jp>
2023-01-21 18:35:02 +0900

    tiny fix, delete error_hundring func

--- app.py
+++ app.py
@@ -8,19 +8,7 @@ app = Flask(__name__)
 def hello_world():
     if request.method == 'POST':
         tweet_run(request.form['tweet_body'])
-        return redirect(url_for('index'))
     return render_template('index.html')
-
-
-@app.errorhandler(500)
-def not_found(error):
-    #エラーJSON作成
-    result = {
-        "error": error,
-        "result":False
-        }
-    #エラーJSONを出力
-    return make_response(jsonify(result), 500)
 
 
 if __name__ == '__main__':
