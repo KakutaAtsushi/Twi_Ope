bc68b60c71af448ef0e49d9cb36da6ff5b1263bc
Atsushi <kakuta.atsushi@outward.jp>
2023-01-21 20:24:55 +0900

    server checking oauth2

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
 from common.tweet import tweet_run
-from common.oauth2 import oauth2
+from common.oauth2 import get_oauth_token, get_access_token
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    oauth_url = oauth2()
+    oauth_url = get_oauth_token()
     if request.method == 'POST':
         tweet_run(request.form['tweet_body'])
     return render_template('index.html', oauth_url=oauth_url)
@@ -16,7 +16,9 @@ def index():
 @app.route('/oauth', methods=['GET'])
 def twitter_auth():
     oauth_token = request.args.get("oauth_token")
-    return render_template('index.html',oauth_token=oauth_token)
+    oauth_verifier = request.args.get("oauth_verifier")
+    screen_name = get_access_token(oauth_token, oauth_verifier)
+    return render_template('index.html', screen_name=screen_name)
 
 
 if __name__ == '__main__':
--- common/oauth2.py
+++ common/oauth2.py
@@ -1,12 +1,11 @@
 from requests_oauthlib import OAuth1Session
-
 from settings import CONFIG
 
+consumer_key = CONFIG["CONSUMER_KEY"]
+consumer_secret = CONFIG["CONSUMER_SECRET"]
 
-def oauth2():
-    consumer_key = CONFIG["CONSUMER_KEY"]
-    consumer_secret = CONFIG["CONSUMER_SECRET"]
 
+def get_oauth_token():
     callback_url = "http://kakip1919.pythonanywhere.com/oauth"
     request_endpoint_url = "https://api.twitter.com/oauth/request_token"
     authenticate_url = "https://api.twitter.com/oauth/authenticate"
@@ -19,3 +18,21 @@ def oauth2():
     token_dict = {x.split("=")[0]: x.split("=")[1] for x in oauth_token_str}
     oauth_token = token_dict["oauth_token"]
     return f"{authenticate_url}?oauth_token={oauth_token}"
+
+
+def get_access_token(oauth_token, oauth_verifier):
+    access_endpoint_url = "https://api.twitter.com/oauth/access_token"
+
+    session_acc = OAuth1Session(consumer_key, consumer_secret, oauth_token, oauth_verifier)
+    response_acc = session_acc.post(access_endpoint_url, params={"oauth_verifier": oauth_verifier})
+    response_acc_text = response_acc.text
+
+    access_token_kvstr = response_acc_text.split("&")
+    acc_token_dict = {x.split("=")[0]: x.split("=")[1] for x in access_token_kvstr}
+    access_token = acc_token_dict["oauth_token"]
+    access_token_secret = acc_token_dict["oauth_token_secret"]
+    return acc_token_dict["screen_name"]
+    print("Access Token       :", access_token)
+    print("Access Token Secret:", access_token_secret)
+    print("User ID            :", acc_token_dict["user_id"])
+    print("Screen Name        :", acc_token_dict["screen_name"])
